166c0a94d45cacb996481e60bfea19b1455627b8
Juan <juan.cadena-m@mail.escuelaing.edu.co>
2025-02-11 14:41:49 -0500

    Primer commit con todos los archivos

---
+++ ejemplo.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+int main(){
+    int x,y;
+    int aux;
+    float height = 1.6;
+    double pi = 3.1416;
+    char character = 'a';
+    bool isStudent = true;
+    
+    std :: cin >> x  >>y;
+    aux = x;
+    y = aux;
+    std :: cout << x << y;
+    //print initialed variables
+    /* 
+    multi
+    line
+    comment 
+    */
+    std :: cout << "height:\t" << height << std :: endl;
+    std :: cout << "pi:\t" << pi << std :: endl;
+    std :: cout << "character:\t" << character << std :: endl;
+    std :: cout << "isStudent:\t" << isStudent << std :: endl;
+   
+}
+   
